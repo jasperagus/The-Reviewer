2f47f1bb0f26c5fd85d042198866814a27c6ecb4
jasper <jasper-05@outlook.com>
2026-03-24 21:31:26 +0100

    Models made

---
+++ TheReviewer/Models/GameModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TheReviewer.Models
+{
+    public class GameModel
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Publisher { get; private set; }
+        public int Score { get; private set; }
+        public DateTime created_at { get; private set; }
+        public DateTime updated_at { get; private set; }
+    }
+}
