3d0dc16297385f299668eea9853a462cd3f49661
jasper <jasper-05@outlook.com>
2026-06-08 10:01:26 +0200

    fix dto deletion

--- TheReviewer.Data/DTOs/GetMediaDTO.cs
+++ TheReviewer.Data/DTOs/GetMediaDTO.cs
@@ -1,3 +1,12 @@
 namespace TheReviewer.Data.DTOs
 {
+    public record GetMediaDTO(
+        int Id,
+        string Name,
+        string Publisher,
+        int Score,
+        int TypeId,
+        DateTime CreatedAt,
+        DateTime UpdatedAt
+    );
 }
